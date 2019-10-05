9c379596405f33d6cf1e9404efd8c35d158f40a3
James-Fire <41241412+James-Fire@users.noreply.github.com>
2019-10-05 12:47:35 -0600

    Add files via upload
    Centrifuge fluid boxes.

---
+++ Factorio-Tiberium_0.1.0/entities.lua
@@ -0,0 +1,24 @@
+local centrifuge = table.deepcopy(data.raw["assembling-machine"]["centrifuge"])
+
+centrifuge.fluid_boxes = 
+{
+	off_when_no_fluid_recipe = true,
+	{
+		production_type = "input",
+		pipe_picture = assembler2pipepictures(),
+		pipe_covers = pipecoverspictures(),
+		base_area = 10,
+		base_level = -1,
+		pipe_connections = {{ type="input", position = {0, -2} }},
+		secondary_draw_orders = { north = -1 }
+	},
+	{
+		production_type = "output",
+		pipe_picture = assembler2pipepictures(),
+		pipe_covers = pipecoverspictures(),
+		base_area = 10,
+		base_level = -1,
+		pipe_connections = {{ type="output", position = {0, 2} }},
+		secondary_draw_orders = { north = -1 }
+	},
+}
