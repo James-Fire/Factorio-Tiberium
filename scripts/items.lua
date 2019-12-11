ce633baa72bca0230681c1b93688218daa5e4b67
Ax <Ax448@users.noreply.github.com>
2019-12-11 20:35:09 +0100

    Mini update, moved sonic wall to tiberium category
    This was mostly to make sure that my repo access still works properly, after updating github username.

--- scripts/items.lua
+++ scripts/items.lua
@@ -4,7 +4,7 @@ data:extend({
         name = "CnC_SonicWall_Hub",
         icon = "__Factorio-Tiberium__/graphics/sonic wall/node icon.png",
 		icon_size = 32,
-        subgroup = "defensive-structure",
+        subgroup = "a-buildings",
         order = "a[stone-wall]-b[hardlight]",
         place_result = "CnC_SonicWall_Hub",
         stack_size = 50
