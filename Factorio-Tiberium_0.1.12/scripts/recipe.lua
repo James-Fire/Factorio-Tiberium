3875e7910c878b2bf8013d6d6b4f26222f66a36a
randomflyingtaco <randomflyingtaco@yahoo.com>
2020-05-28 17:47:44 -0500

    Splitting Techs, Simplifying Recipes, Earlier Fuge
    Major changes to Tiberium Centrifuge recipes to allow earlier Tiberium Separation and Tiberium Processing
    Tiberium Separation no longer requires Green Science
    Tiberium Processing no longer requires Blue Science
    Minor technology cost adjustments to Molten Tiberium Processing, Liquid Tiberium Power, TCN, Nuclear Testing, EM Testing
    Moved Tiberium Farming from Growth Acceleration to TCN, made Growth Accleration significantly cheaper and no longer require Purple Science or Nuclear Testing
    Split SRF+Node Harvester, Tiberium Armor+Tiberium Magazine, Ion Core+Ion Turret+Tiberium Core Missile, and Tiberium Seed+Tiberium Bomb to be 4 separate techs instead of squished in with unrelated techs.
    Recipe changes for Tiberium Spike, TCN Node, Tiberium Magazine, Tiberium Armor, Tiberium Nuclear Fuel, Tiberium Fuel Cells, Ion Core, Ion Beam Projector, and Tiberium Seed

--- Factorio-Tiberium_0.1.12/scripts/recipe.lua
+++ Factorio-Tiberium_0.1.12/scripts/recipe.lua
@@ -139,8 +139,8 @@ data:extend {
     subgroup = "a-items",
 	fuel_category = "nuclear",
     burnt_result = "tiberium-dirty-cell",
-	fuel_value = "1GJ",
-    stack_size = 5
+	fuel_value = "10GJ",
+    stack_size = 50
   },
   {
     type = "item",
@@ -184,8 +184,8 @@ data:extend {
 }
 
 data:extend(
---Science stuff. Making data, turning it into science, learning from farming tib
   {
+--Science stuff. Making data, turning it into science, learning from farming tib
 	{
       type = "recipe",
       name = "tiberium-farming",
@@ -685,7 +685,7 @@ data:extend(
       name = "tiberium-sludge-to-stone-brick",
       category = "chemistry",
 	  crafting_machine_tint = TibCraftingTint,
-      energy_required = 4,
+      energy_required = 2,
       enabled = false,
       ingredients = {
         {type = "fluid", name = "tiberium-sludge", amount = 1}
@@ -698,6 +698,7 @@ data:extend(
       subgroup = "a-direct",
 	  allow_as_intermediate = false,
 	  allow_decomposition = false,
+	  always_show_made_in = true,
       order = "z"
     },
 	{
@@ -735,60 +736,9 @@ data:extend(
       subgroup = "a-direct",
 	  allow_as_intermediate = false,
 	  allow_decomposition = false,
+	  always_show_made_in = true,
       order = "c"
     },
-    {
-      type = "recipe",
-      name = "tiberium-molten-to-stone",
-      category = "chemistry",
-	  crafting_machine_tint = TibCraftingTint,
-      energy_required = 4,
-      enabled = false,
-      ingredients = {
-      },
-      results = {
-      },
-      icon_size = 32,
-      subgroup = "a-direct",
-	  allow_as_intermediate = false,
-	  allow_decomposition = false,
-      order = "c"
-    },
-    {
-      type = "recipe",
-      name = "tiberium-molten-to-coal",
-      category = "chemistry",
-	  crafting_machine_tint = TibCraftingTint,
-      energy_required = 4,
-      enabled = false,
-      ingredients = {
-      },
-      results = {
-      },
-      icon_size = 32,
-      subgroup = "a-direct",
-	  allow_as_intermediate = false,
-	  allow_decomposition = false,
-      order = "d"
-    },
-    {
-      type = "recipe",
-      name = "tiberium-molten-to-crude-oil",
-      category = "chemistry",
-	  crafting_machine_tint = TibCraftingTint,
-      energy_required = 4,
-      enabled = false,
-      ingredients = {
-      },
-      results = {
-      },
-      icon_size = 32,
-      subgroup = "a-direct",
-	  allow_as_intermediate = false,
-	  allow_decomposition = false,
-      order = "e"
-    },
-	
 --Centrifuging Recipes
     {
       type = "recipe",
@@ -806,6 +756,7 @@ data:extend(
       icon_size = 32,
 	  allow_as_intermediate = false,
 	  allow_decomposition = false,
+	  always_show_made_in = true,
       order = "a[fluid-chemistry]-f[heavy-oil-cracking]"
     },
     {
@@ -813,7 +764,7 @@ data:extend(
       name = "tiberium-slurry-centrifuging",
       category = "tiberium-centrifuge-2",
 	  subgroup = "a-centrifuging",
-      energy_required = 8,
+      energy_required = 15,
       enabled = false,
       ingredients = {
       },
@@ -823,6 +774,7 @@ data:extend(
       icon_size = 32,
 	  allow_as_intermediate = false,
 	  allow_decomposition = false,
+	  always_show_made_in = true,
       order = "b[fluid-chemistry]-f[heavy-oil-cracking]"
     },
     {
@@ -830,7 +782,7 @@ data:extend(
       name = "tiberium-molten-centrifuging",
       category = "tiberium-centrifuge-3",
 	  subgroup = "a-centrifuging",
-      energy_required = 6,
+      energy_required = 20,
       enabled = false,
       ingredients = {
       },
@@ -840,6 +792,7 @@ data:extend(
       icon_size = 32,
 	  allow_as_intermediate = false,
 	  allow_decomposition = false,
+	  always_show_made_in = true,
       order = "c[fluid-chemistry]-f[heavy-oil-cracking]"
     },
 	{
@@ -858,6 +811,7 @@ data:extend(
       icon_size = 32,
 	  allow_as_intermediate = false,
 	  allow_decomposition = false,
+	  always_show_made_in = true,
       order = "d"
     },
     {
@@ -865,7 +819,7 @@ data:extend(
       name = "tiberium-slurry-sludge-centrifuging",
       category = "tiberium-centrifuge-2",
 	  subgroup = "a-centrifuging",
-      energy_required = 8,
+      energy_required = 15,
       enabled = false,
       ingredients = {
       },
@@ -875,6 +829,7 @@ data:extend(
       icon_size = 32,
 	  allow_as_intermediate = false,
 	  allow_decomposition = false,
+	  always_show_made_in = true,
       order = "e"
     },
     {
@@ -882,7 +837,7 @@ data:extend(
       name = "tiberium-molten-sludge-centrifuging",
       category = "tiberium-centrifuge-3",
 	  subgroup = "a-centrifuging",
-      energy_required = 6,
+      energy_required = 20,
       enabled = false,
       ingredients = {
       },
@@ -892,6 +847,7 @@ data:extend(
       icon_size = 32,
 	  allow_as_intermediate = false,
 	  allow_decomposition = false,
+	  always_show_made_in = true,
       order = "f"
     },
 --Growth Credits
@@ -937,19 +893,15 @@ data:extend(
 --Power recipes
 	{
       type = "recipe",
-      name = "liquid-tiberium-cell",
+      name = "tiberium-nuclear-fuel",
 	  crafting_machine_tint = TibCraftingTint,
       category = "chemistry",
 	  subgroup = "a-items",
       energy_required = 20,
       enabled = false,
       ingredients = {
-		{type = "fluid", name = "liquid-tiberium", amount = 25},
-		{type = "item", name = "steel-plate", amount = 2},
-		{type = "item", name = "copper-plate", amount = 2},
-        {type = "item", name = "electric-engine-unit", amount = 1},
-        {type = "item", name = "processing-unit", amount = 2},
-        {type = "item", name = "pipe", amount = 1},
+		{type = "item", name = "solid-fuel", amount = 10},
+		{type = "fluid", name = "liquid-tiberium", amount = 10},
       },
       results = {
         {type = "item", name = "nuclear-fuel", amount = 1},
@@ -1010,7 +962,7 @@ data:extend(
       enabled = false,
       ingredients = {
 		{type = "item", name = "tiberium-empty-cell", amount = 1},
-		{type = "fluid", name = "liquid-tiberium", amount = 10},
+		{type = "fluid", name = "liquid-tiberium", amount = 40},
       },
       results = {
         {type = "item", name = "tiberium-fuel-cell", amount = 1},
@@ -1018,7 +970,6 @@ data:extend(
       icon_size = 64,
       order = "b[fluid-chemistry]-f[heavy-oil-cracking]"
     },
-	
 	
 --Intermediate Products
 	{
@@ -1030,11 +981,9 @@ data:extend(
       energy_required = 20,
       enabled = false,
       ingredients = {
-		{type = "fluid", name = "liquid-tiberium", amount = 1},
-		{type = "item", name = "steel-plate", amount = 2},
-		{type = "item", name = "copper-plate", amount = 2},
-        {type = "item", name = "processing-unit", amount = 2},
-        {type = "item", name = "pipe", amount = 1},
+		{type = "fluid", name = "liquid-tiberium", amount = 10},
+		{type = "item", name = "steel-plate", amount = 5},
+        {type = "item", name = "pipe", amount = 5},
       },
       results = {
         {type = "item", name = "tiberium-ion-core", amount = 1},
--- Factorio-Tiberium_0.1.12/scripts/technology.lua
+++ Factorio-Tiberium_0.1.12/scripts/technology.lua
@@ -25,12 +25,8 @@ data:extend
 				type = "unlock-recipe",
 				recipe = "tiberium-sludge-to-stone-brick"
 			},
-			{
-				type = "unlock-recipe",
-				recipe = "tiberium-armor"
-			},
 		},
-		prerequisites = {"tiberium-mechanical-research", "concrete", "engine"},
+		prerequisites = {"tiberium-mechanical-research", "steel-processing"},
 		unit =
 		{
 			count = 100,
@@ -38,7 +34,6 @@ data:extend
 			{
 				{"tiberium-science", 1},
 				{"automation-science-pack", 1},
-				{"logistic-science-pack", 1},
 			},
 			time = 30
 		}
@@ -46,8 +41,8 @@ data:extend
 	{
 		type = "technology",
 		name = "tiberium-processing-tech",
-		icon = "__base__/graphics/technology/oil-gathering.png",
-		icon_size = 128,
+		icon = "__Factorio-Tiberium__/graphics/icons/fluid/tiberium-waste.png",
+		icon_size = 64,
 		effects =
 		{
 			{
@@ -70,29 +65,16 @@ data:extend
 				type = "unlock-recipe",
 				recipe = "tiberium-slurry-mechanical-data"
 			},
-			{
-				type = "unlock-recipe",
-				recipe = "tiberium-magazine"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "CnC_SonicWall_Hub"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "tib-pumpjack"
-			},
 		},
-		prerequisites = {"tiberium-separation-tech", "chemical-science-pack"},
+		prerequisites = {"tiberium-separation-tech", "advanced-electronics", "concrete"},
 		unit =
 		{
-			count = 500,
+			count = 400,
 			ingredients =
 			{
 				{"tiberium-science", 1},
 				{"automation-science-pack", 1},
 				{"logistic-science-pack", 1},
-				{"chemical-science-pack", 1}
 			},
 			time = 30
 		}
@@ -100,8 +82,8 @@ data:extend
 	{
 		type = "technology",
 		name = "tiberium-molten-processing",
-		icon = "__Factorio-Tiberium__/graphics/technology/tiberium-processing-tech.png",
-		icon_size = 128,
+		icon = "__Factorio-Tiberium__/graphics/icons/fluid/molten-tiberium.png",
+		icon_size = 64,
 		effects =
 		{
 			{
@@ -136,7 +118,7 @@ data:extend
 		prerequisites = {"tiberium-processing-tech", "tiberium-thermal-research"},
 		unit =
 		{
-			count = 1000,
+			count = 800,
 			ingredients =
 			{
 				{"tiberium-science", 1},
@@ -178,29 +160,47 @@ data:extend
 				type = "unlock-recipe",
 				recipe = "tiberium-waste-recycling"
 			},
-			{
-				type = "unlock-recipe",
-				recipe = "tiberium-rocket"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "tiberium-ion-core"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "ion-turret"
-			}
 		},
 		prerequisites = {"tiberium-molten-processing", "tiberium-chemical-research"},
 		unit =
 		{
-			count = 1500,
+			count = 1200,
 			ingredients =
 			{
 				{"tiberium-science", 1},
 				{"automation-science-pack", 1},
 				{"logistic-science-pack", 1},
 				{"chemical-science-pack", 1}
+			},
+			time = 30
+		}
+	},
+	{
+		type = "technology",
+		name = "tiberium-containment-tech",
+		icon = "__Factorio-Tiberium__/graphics/sonic wall/node icon.png",
+		icon_size = 32,
+		effects =
+		{
+			{
+				type = "unlock-recipe",
+				recipe = "CnC_SonicWall_Hub"
+			},
+			{
+				type = "unlock-recipe",
+				recipe = "tib-pumpjack"
+			},
+		},
+		prerequisites = {"tiberium-processing-tech", "battery"},
+		unit =
+		{
+			count = 200,
+			ingredients =
+			{
+				{"tiberium-science", 1},
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
 			},
 			time = 30
 		}
@@ -243,7 +243,7 @@ data:extend
 	{
 		type = "technology",
 		name = "tiberium-growth-acceleration",
-		icon = "__base__/graphics/technology/oil-gathering.png",
+		icon = "__Factorio-Tiberium__/graphics/technology/growth-accelerator.png",
 		icon_size = 128,
 		effects =
 		{
@@ -251,22 +251,17 @@ data:extend
 				type = "unlock-recipe",
 				recipe = "growth-accelerator"
 			},
-			{
-				type = "unlock-recipe",
-				recipe = "tiberium-farming"
-			},
 		},
-		prerequisites = {"tiberium-nuclear-research", "tiberium-processing-tech"},
+		prerequisites = {"tiberium-processing-tech"},
 		unit =
 		{
-			count = 2500,
+			count = 800,
 			ingredients =
 			{
 				{"tiberium-science", 1},
 				{"automation-science-pack", 1},
 				{"logistic-science-pack", 1},
 				{"chemical-science-pack", 1},
-				{"production-science-pack", 1},
 			},
 			time = 30
 		}
@@ -280,11 +275,7 @@ data:extend
 		{
 			{
 				type = "unlock-recipe",
-				recipe = "liquid-tiberium-cell"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "energy-growth-credit"
+				recipe = "tiberium-network-node"
 			},
 			{
 				type = "unlock-recipe",
@@ -292,21 +283,21 @@ data:extend
 			},
 			{
 				type = "unlock-recipe",
-				recipe = "tiberium-network-node"
+				recipe = "tiberium-farming"
 			},
 			{
 				type = "unlock-recipe",
-				recipe = "tiberium-seed"
+				recipe = "energy-growth-credit"
 			},
 			{
 				type = "unlock-recipe",
-				recipe = "tiberium-nuke"
+				recipe = "tiberium-nuclear-fuel"
 			},
 		},
 		prerequisites = {"tiberium-power-tech", "tiberium-electromagnetic-research"},
 		unit =
 		{
-			count = 2500,
+			count = 2400,
 			ingredients =
 			{
 				{"tiberium-science", 1},
@@ -319,8 +310,7 @@ data:extend
 			time = 30
 		}
 	}, 
-
-	--Samples
+	--Science Techs
 	{
 		type = "technology",
 		name = "tiberium-mechanical-research",
@@ -436,7 +426,7 @@ data:extend
 		prerequisites = {"tiberium-chemical-research", "uranium-processing", "production-science-pack"},
 		unit =
 		{
-			count = 500,
+			count = 300,
 			ingredients =
 			{
 				{"automation-science-pack", 1},
@@ -474,7 +464,7 @@ data:extend
 		prerequisites = {"tiberium-nuclear-research", "utility-science-pack"},
 		unit =
 		{
-			count = 500,
+			count = 400,
 			ingredients =
 			{
 				{"automation-science-pack", 1},
@@ -486,7 +476,104 @@ data:extend
 			time = 30
 		}
 	},
-	 
+	--Military
+	{
+		type = "technology",
+		name = "tiberium-military-1",
+		icon = "__Factorio-Tiberium__/graphics/technology/tiberium-military.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "unlock-recipe",
+				recipe = "tiberium-magazine"
+			},
+			{
+				type = "unlock-recipe",
+				recipe = "tiberium-armor"
+			},
+		},
+		prerequisites = {"tiberium-separation-tech", "military-science-pack"},
+		unit =
+		{
+			count = 100,
+			ingredients =
+			{
+				{"tiberium-science", 1},
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"military-science-pack", 1},
+			},
+			time = 30
+		}
+	},
+	{
+		type = "technology",
+		name = "tiberium-military-2",
+		icon = "__Factorio-Tiberium__/graphics/technology/tiberium-military.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "unlock-recipe",
+				recipe = "tiberium-ion-core"
+			},
+			{
+				type = "unlock-recipe",
+				recipe = "ion-turret"
+			},
+			{
+				type = "unlock-recipe",
+				recipe = "tiberium-rocket"
+			},
+		},
+		prerequisites = {"tiberium-military-1", "tiberium-power-tech", "rocketry"},
+		unit =
+		{
+			count = 300,
+			ingredients =
+			{
+				{"tiberium-science", 1},
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
+				{"military-science-pack", 1},
+			},
+			time = 30
+		}
+	},
+	{
+		type = "technology",
+		name = "tiberium-military-3",
+		icon = "__Factorio-Tiberium__/graphics/technology/tiberium-military.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "unlock-recipe",
+				recipe = "tiberium-nuke"
+			},
+			{
+				type = "unlock-recipe",
+				recipe = "tiberium-seed"
+			},
+		},
+		prerequisites = {"tiberium-military-2", "rocket-control-unit", "tiberium-control-network-tech"},
+		unit =
+		{
+			count = 500,
+			ingredients =
+			{
+				{"tiberium-science", 1},
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"military-science-pack", 1},
+				{"chemical-science-pack", 1},
+				{"utility-science-pack", 1},
+			},
+			time = 30
+		}
+	},
 	--Repeatables
 	{
 		type = "technology",
