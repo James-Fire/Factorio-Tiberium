b1baf6c63ab244b31da32ed2f60fb8e50dc8ebf3
randomflyingtaco <randomflyingtaco@yahoo.com>
2021-06-18 12:30:35 -0500

    1.1.16 Improve Formatting Consistency
    No functional changes

--- Factorio-Tiberium/prototype/entity/tiberium-beacon-node.lua
+++ Factorio-Tiberium/prototype/entity/tiberium-beacon-node.lua
@@ -3,88 +3,88 @@ local sounds = require("__base__.prototypes.entity.sounds")
 local TiberiumRadius = 20 + settings.startup["tiberium-spread"].value * 0.4 --Translates to 20-60 range
 
 
-data:extend({
+data:extend{
 	{
-	type = "beacon",
-	name = "tiberium-beacon-node",
-	icon = tiberiumInternalName.."/graphics/icons/beacon.png",
-	icon_size = 32,
-	flags = {"placeable-player", "player-creation"},
-	minable = {mining_time = 0.2, result = "tiberium-beacon-node"},
-	max_health = 200,
-	corpse = "beacon-remnants",
-	dying_explosion = "beacon-explosion",
-	collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-	selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-	damaged_trigger_effect = hit_effects.entity(),
-	drawing_box = {{-1.5, -2.2}, {1.5, 1.3}},
-	allowed_effects = {"consumption"},
-	animation = {
-		animation_speed = 0.5,
-		filename = tiberiumInternalName.."/graphics/entity/beacon/beacon-antenna.png",
-		frame_count = 32,
-		height = 50,
-		width = 54,
-		line_length = 8,
-		shift = {-0.03125, -1.71875},
-	},
-	base_picture = {
-		filename = tiberiumInternalName.."/graphics/entity/beacon/beacon-base.png",
-		height = 93,
-		width = 116,
-		shift = {0.34375, 0.046875},
-	},
-	radius_visualisation_picture = {
-		filename = "__base__/graphics/entity/beacon/beacon-radius-visualization.png",
-		priority = "extra-high-no-scale",
-		width = 10,
-		height = 10
-	},
-	supply_area_distance = math.floor(TiberiumRadius * 0.5),
-	energy_source = {
-		type = "void",
-		usage_priority = "secondary-input"
-	},
-	vehicle_impact_sound = sounds.generic_impact,
-	open_sound = sounds.machine_open,
-	close_sound = sounds.machine_close,
-	working_sound = {
-		sound = {
-			{
-			filename = "__base__/sound/beacon-1.ogg",
-			volume = 0.2
+		type = "beacon",
+		name = "tiberium-beacon-node",
+		icon = tiberiumInternalName.."/graphics/icons/beacon.png",
+		icon_size = 32,
+		flags = {"placeable-player", "player-creation"},
+		minable = {mining_time = 0.2, result = "tiberium-beacon-node"},
+		max_health = 200,
+		corpse = "beacon-remnants",
+		dying_explosion = "beacon-explosion",
+		collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
+		selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+		damaged_trigger_effect = hit_effects.entity(),
+		drawing_box = {{-1.5, -2.2}, {1.5, 1.3}},
+		allowed_effects = {"consumption"},
+		animation = {
+			animation_speed = 0.5,
+			filename = tiberiumInternalName.."/graphics/entity/beacon/beacon-antenna.png",
+			frame_count = 32,
+			height = 50,
+			width = 54,
+			line_length = 8,
+			shift = {-0.03125, -1.71875},
+		},
+		base_picture = {
+			filename = tiberiumInternalName.."/graphics/entity/beacon/beacon-base.png",
+			height = 93,
+			width = 116,
+			shift = {0.34375, 0.046875},
+		},
+		radius_visualisation_picture = {
+			filename = "__base__/graphics/entity/beacon/beacon-radius-visualization.png",
+			priority = "extra-high-no-scale",
+			width = 10,
+			height = 10
+		},
+		supply_area_distance = math.floor(TiberiumRadius * 0.5),
+		energy_source = {
+			type = "void",
+			usage_priority = "secondary-input"
+		},
+		vehicle_impact_sound = sounds.generic_impact,
+		open_sound = sounds.machine_open,
+		close_sound = sounds.machine_close,
+		working_sound = {
+			sound = {
+				{
+				filename = "__base__/sound/beacon-1.ogg",
+				volume = 0.2
+				},
+				{
+				filename = "__base__/sound/beacon-2.ogg",
+				volume = 0.2
+				}
 			},
-			{
-			filename = "__base__/sound/beacon-2.ogg",
-			volume = 0.2
-			}
+			audible_distance_modifier = 0.33,
+			max_sounds_per_type = 3
+			-- fade_in_ticks = 4,
+			-- fade_out_ticks = 60
 		},
-		audible_distance_modifier = 0.33,
-		max_sounds_per_type = 3
-		-- fade_in_ticks = 4,
-		-- fade_out_ticks = 60
-	},
-	energy_usage = "10MW",
-	distribution_effectivity = 0,
-	module_specification = {
-		module_slots = 0,
-		module_info_icon_shift = {0, 0},
-		module_info_multi_row_initial_height_modifier = -0.3,
-		module_info_max_icons_per_row = 2,
-	},
-	water_reflection = {
-		pictures = {
-			filename = "__base__/graphics/entity/beacon/beacon-reflection.png",
-			priority = "extra-high",
-			width = 24,
-			height = 28,
-			shift = util.by_pixel(0, 55),
-			variation_count = 1,
-			scale = 5,
+		energy_usage = "10MW",
+		distribution_effectivity = 0,
+		module_specification = {
+			module_slots = 0,
+			module_info_icon_shift = {0, 0},
+			module_info_multi_row_initial_height_modifier = -0.3,
+			module_info_max_icons_per_row = 2,
 		},
-		rotate = false,
-		orientation_to_variation = false
-	}
+		water_reflection = {
+			pictures = {
+				filename = "__base__/graphics/entity/beacon/beacon-reflection.png",
+				priority = "extra-high",
+				width = 24,
+				height = 28,
+				shift = util.by_pixel(0, 55),
+				variation_count = 1,
+				scale = 5,
+			},
+			rotate = false,
+			orientation_to_variation = false
+		}
 	},
 	--Invisible beacons for Growth Accelerator speed research
 	{
@@ -151,4 +151,4 @@ data:extend({
 		selection_box = {{0, 0}, {0, 0}},
 		collision_box = {{-0.4, -0.4}, {0.4, 0.4}}, -- reduce size preventing inserters from picking modules, will not power unless center is covered
 	},
-})
+}
