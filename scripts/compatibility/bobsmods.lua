82f73db17b975431c49cc47f4e3807172eece80e
James-Fire <41241412+James-Fire@users.noreply.github.com>
2025-03-03 13:08:58 -0700

    Include bob- prefix on bob items

--- scripts/compatibility/bobsmods.lua
+++ scripts/compatibility/bobsmods.lua
@@ -4,8 +4,8 @@ if mods["bobores"] then
 end
 
 if mods["bobplates"] then
-	if data.raw.item["gas-canister"] then
-		data.raw.item["gas-canister"].tiberium_empty_barrel = true
+	if data.raw.item["bob-gas-canister"] then
+		data.raw.item["bob-gas-canister"].tiberium_empty_barrel = true
 	end
 end
 
@@ -17,7 +17,7 @@ if data.raw.item["bob-mining-drill-3"] then
 	common.recipe.editIngredient("tiberium-network-node", "electric-mining-drill", "bob-mining-drill-3")
 end
 --Titanium
-if data.raw.item["titanium-plate"] then
+if data.raw.item["bob-titanium-plate"] then
 	common.recipe.editIngredient("tiberium-ion-core", "steel-plate", "titanium-plate")
 	common.recipe.editIngredient("tiberium-marv", "steel-plate", "titanium-plate")
 	common.recipe.editIngredient("tiberium-power-plant", "steel-plate", "titanium-plate")
@@ -25,42 +25,42 @@ if data.raw.item["titanium-plate"] then
 	common.recipe.editIngredient("tiberium-aoe-node-harvester", "steel-plate", "titanium-plate")
 end
 --Gold
-if data.raw.item["gold-plate"] then
-	common.recipe.editIngredient("tiberium-beacon-node", "copper-plate", "gold-plate")
-	common.recipe.editIngredient("tiberium-srf-emitter", "copper-plate", "gold-plate")
+if data.raw.item["bob-gold-plate"] then
+	common.recipe.editIngredient("tiberium-beacon-node", "copper-plate", "bob-gold-plate")
+	common.recipe.editIngredient("tiberium-srf-emitter", "copper-plate", "bob-gold-plate")
 end
 --Steel Pipe
-if data.raw.item["steel-pipe"] then
+if data.raw.item["bob-steel-pipe"] then
 	common.recipe.editIngredient("tiberium-growth-accelerator", "pipe", "steel-pipe")
 	common.recipe.editIngredient("tiberium-network-node", "pipe", "steel-pipe")
 	common.recipe.editIngredient("tiberium-ion-core", "pipe", "steel-pipe")
 end
 --Aluminum
-if data.raw.item["aluminium-plate"] then
+if data.raw.item["bob-aluminium-plate"] then
 	common.recipe.editIngredient("tiberium-growth-accelerator", "steel-plate", "aluminium-plate")
 	common.recipe.editIngredient("tiberium-srf-emitter", "steel-plate", "aluminium-plate")
 	common.recipe.editIngredient("tiberium-obelisk-of-light", "steel-plate", "aluminium-plate")
 	common.recipe.editIngredient("tiberium-node-harvester", "iron-plate", "aluminium-plate")
 end
 --Lead
-if data.raw.item["lead-plate"] then
+if data.raw.item["bob-lead-plate"] then
 	common.recipe.editIngredient("tiberium-empty-cell", "steel-plate", "lead-plate")
 end
 --Beacon 3
-if data.raw.item["beacon-3"] then
+if data.raw.item["bob-beacon-3"] then
 	common.recipe.editIngredient("tiberium-beacon-node", "beacon", "beacon-3")
 end
 --Advanced Processing Units
-if data.raw.item["advanced-processing-unit"] then
+if data.raw.item["bob-advanced-processing-unit"] then
 	common.recipe.editIngredient("tiberium-beacon-node", "processing-unit", "advanced-processing-unit")
 	common.recipe.editIngredient("tiberium-spike", "processing-unit", "advanced-processing-unit")
 end
 --Chemical Plant 2
-if data.raw.item["chemical-plant-2"] then
+if data.raw.item["bob-chemical-plant-2"] then
 	common.recipe.editIngredient("tiberium-power-plant", "chemical-plant", "chemical-plant-2")
 end
 --Solar Panel 3
-if data.raw.item["solar-panel-3"] then
+if data.raw.item["bob-solar-panel-3"] then
 	common.recipe.editIngredient("tiberium-spike", "solar-panel", "solar-panel-3")
 end
 --Pumpjack 4
